3af45d4650496eb8d8756aca87ee1ce73a48e3eb
Gabriel Resende <93008823+GBResende@users.noreply.github.com>
2022-03-17 20:48:57 -0300

    [group 29 - Gabs]req 17 (#704)
    * Iniciando o projeto.
    
    * Requirement 1 complete.
    
    * Lint fixed.
    
    * Started Requirement 12 but application is broken.
    
    * Application doesn't break anymore.
    
    * create global state with hashEmail
    
    * component HeaderInGame Done
    
    * fix errors in component HeaderInGame
    
    * fix lint
    
    * create outline of feedback page
    
    * fix lint
    
    * requirement 5 is done finally
    
    * refatoring requirement 5
    
    * requirement 13 is done
    
    * Temporizador infinito feito.
    
    * Reducers are now organized.
    
    * Timer stops on zero. (10 for testing)
    
    * Timer's reducer and action done.
    
    * First question's answers get disabled after 30s.
    
    * Requirement 9 completed
    
    * testing requirement 10
    
    * return to requirement 9
    
    * ending requirement 12
    
    * fix showScore
    
    * Requirement 12 Fixed
    
    * requirement 14 is done
    
    * fix requiremente 14
    
    * fix requirement 14 again
    
    * fix requirement 14, update reducer
    
    * requirement 15 is done
    
    * fix word play again
    
    * put button play again on feedback page
    
    * put handleClickPlayAgain on feedback page
    
    * update feedback page
    
    * requirement 17 is done
    
    Co-authored-by: AlissaMaximo <alissahiratamaximo@gmail.com>
    Co-authored-by: Aline Salema <alineelainesalema@gmail.com>

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,30 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
+  handleClickGoHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <header>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          onClick={ this.handleClickGoHome }
+          type="button"
+          data-testid="btn-go-home"
+        >
+          Go Home
+        </button>
       </header>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Ranking;
